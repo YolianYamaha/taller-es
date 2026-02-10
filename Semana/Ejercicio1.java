ed1bdc53c2725e963c39da39c80895ce0b6911d8
YolianYamaha <yandresbarona@estudiante.uniajc.edu.co>
2026-02-10 16:27:21 +0000

    Primer ejercicio con arreglos

---
+++ Semana/Ejercicio1.java
@@ -0,0 +1,15 @@
+package semana2;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        // Formato -> Definiendo directamente los valores del arreglo
+        int[] a = {4, 8, 9, 6, 1, 2};
+
+        // Iterar los elementos del arreglo
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "]=" + a[i]);
+        }
+    }
+}
